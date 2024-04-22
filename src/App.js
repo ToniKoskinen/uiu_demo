007529c66cd8d8eb7e74522d8af6e8843e8e9ff2
tonska112 <toni.viljami.koskinen@gmail.com>
2024-04-22 10:53:55 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Footer from "./components/Footer"
@@ -6,14 +6,14 @@ import Dropdown from "./components/Dropdown"
 import Collection from './components/Collection';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header></Header>
-      <Dropdown></Dropdown>
-      <Collection></Collection>
-      <Footer></Footer>
-      
+      <Routes>
+        <Route path="/" element={[<Header></Header>, <Dropdown></Dropdown>,<Collection></Collection>,<Footer></Footer>]}></Route>
+      </Routes>
       
     </div>
+    </Router>
   );
 }
 
